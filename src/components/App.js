fcb0911d473a54c2de778cb481ed13f83af16838
SOURAV-ROY <souravroy.info13@gmail.com>
2021-11-01 23:10:54 +0600

    NavigationBar Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
-import Home from './Home';
+import '../components/assets/css/App.css';
+import Home from './Home/Home';
+import Nav from "./Nav/Nav";
 
 function App() {
     return (
         <div className="App">
+            <Nav/>
             <Home/>
             <h1>Quiz App By React</h1>
             <h1>Quiz App By React</h1>
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Account from "../Account/Account";
+import sou from '../assets/css/Nav.module.css'
+import logo from '../assets/images/logo-bg.png';
+
+function Nav(props) {
+    return (
+        <nav className={sou.nav}>
+            <ul>
+                <li>
+                    <a href="index.html" className={sou.brand}>
+                        <img src={logo} alt="SOURAV"/>
+                        <h3>Learn with SOURAV</h3>
+                    </a>
+                </li>
+            </ul>
+            <Account/>
+        </nav>
+    );
+}
+
+export default Nav;
