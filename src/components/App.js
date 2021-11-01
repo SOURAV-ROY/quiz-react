6dae2528115e847d77433d1bacf4b0df4795bb98
SOURAV-ROY <souravroy.info13@gmail.com>
2021-11-01 18:21:01 +0600

    Project Setup Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Home from './Home';
 
 function App() {
-
     return (
         <div className="App">
+            <Home/>
+            <h1>Quiz App By React</h1>
             <h1>Quiz App By React</h1>
         </div>
     );
