62e743c0c76b2d98a07289789da610051583b4a0
SOURAV-ROY <souravroy.info13@gmail.com>
2021-11-05 15:40:49 +0600

    Some Typo Fix with CSS update

--- src/components/Pages/Quiz/Answers/Answers.js
+++ src/components/Pages/Quiz/Answers/Answers.js
@@ -5,7 +5,16 @@ import sou from './Answers.module.css';
 function Answers() {
     return (
         <div className={sou.answers}>
-            <Checkbox className={sou.answer} text='Test Answers'/>
+            <Checkbox className={sou.answer} text='Test Answer 1'/>
+            <Checkbox className={sou.answer} text='Test Answer 2'/>
+            <Checkbox className={sou.answer} text='Test Answer 3'/>
+            <Checkbox className={sou.answer} text='Test Answer 4'/>
+            <Checkbox className={sou.answer} text='Test Answer 5'/>
+            <Checkbox className={sou.answer} text='Test Answer 6'/>
+            <Checkbox className={sou.answer} text='Test Answer 7'/>
+            <Checkbox className={sou.answer} text='Test Answer 8'/>
+            <Checkbox className={sou.answer} text='Test Answer 9'/>
+            <Checkbox className={sou.answer} text='Test Answer 10'/>
         </div>
     );
 }
