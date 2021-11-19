ca4dc851d29afab75d4b386d40ffad0246cccae4
SOURAV-ROY <souravroy.info13@gmail.com>
2021-11-19 14:32:18 +0600

    Questions & Answer Get From Firebase 💝

--- src/components/Pages/Quiz/Answers/Answers.js
+++ src/components/Pages/Quiz/Answers/Answers.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Checkbox from "../../../Checkbox/Checkbox";
 import sou from './Answers.module.css';
 
-function Answers() {
+function Answers({options = [], handleChange}) {
+
     return (
         <div className={sou.answers}>
-            <Checkbox className={sou.answer} text='Test Answer 1'/>
-            <Checkbox className={sou.answer} text='Test Answer 2'/>
-            <Checkbox className={sou.answer} text='Test Answer 3'/>
-            <Checkbox className={sou.answer} text='Test Answer 4'/>
-            <Checkbox className={sou.answer} text='Test Answer 5'/>
-            <Checkbox className={sou.answer} text='Test Answer 6'/>
-            <Checkbox className={sou.answer} text='Test Answer 7'/>
-            <Checkbox className={sou.answer} text='Test Answer 8'/>
-            <Checkbox className={sou.answer} text='Test Answer 9'/>
-            <Checkbox className={sou.answer} text='Test Answer 10'/>
+            {options.map((option, index) => (
+                <Checkbox
+                    className={sou.answer}
+                    text={option.title}
+                    value={index}
+                    key={index}
+                    checked={option.checked}
+                    onChange={(e) => {
+                        handleChange(e, index);
+                    }}
+                />
+            ))}
         </div>
     );
 }
--- src/components/Pages/Quiz/Quiz.js
+++ src/components/Pages/Quiz/Quiz.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 import Answers from "./Answers/Answers";
 import MiniPlayer from "./MiniPlayer/MiniPlayer";
 import ProgressBar from "./ProgressBar/ProgressBar";
+import useQuestions from '../../../hooks/useQuestions';
+import {useParams} from "react-router-dom";
+import _ from 'lodash';
+
+const initialState = null;
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'questions':
+            action.value.forEach((question) => {
+                question.options.forEach((option) => {
+                    option.checked = false;
+                });
+            });
+            return action.value;
+
+        case 'answer':
+            const questions = _.cloneDeep(state);
+            questions[action.questionID].options[action.optionIndex].checked = action.value;
+            return questions;
+
+        default:
+            return state;
+    }
+}
 
 function Quiz() {
+    const {id} = useParams();
+    const {loading, error, questions} = useQuestions(id);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const [qna, dispatch] = useReducer(reducer, initialState);
+    console.log(qna);
+
+    useEffect(() => {
+        dispatch({
+            type: 'questions',
+            value: questions
+        })
+    }, [questions]);
+
+    const handleAnswerChange = (e, index) => {
+        dispatch({
+            type: 'answer',
+            questionID: currentQuestion,
+            optionIndex: index,
+            value: e.target.checked,
+        })
+    }
     return (
         <>
-            <h1>Pick three of your favorite Star Wars Films</h1>
-            <h4>Question can have multiple answers</h4>
-            <Answers/>
-            <ProgressBar/>
-            <MiniPlayer/>
+            {loading && <div>Loading....</div>}
+            {error && <div>There was an error</div>}
+
+            {!loading && !error && qna && qna.length > 0 && (
+                <>
+                    <h1>{qna[currentQuestion].title}</h1>
+                    <h4>Question can have multiple answers</h4>
+                    <Answers options={qna[currentQuestion].options} handleChange={handleAnswerChange}/>
+                    <ProgressBar/>
+                    <MiniPlayer/>
+                </>
+            )}
         </>
     );
 }
