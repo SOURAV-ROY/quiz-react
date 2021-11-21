fe1cbcb5d621a56b88cb65cf822904a169d93f0d
SOURAV-ROY <souravroy.info13@gmail.com>
2021-11-21 13:55:06 +0600

    ToggleMiniPlayer Is DONE

--- src/components/Pages/Quiz/MiniPlayer/MiniPlayer.js
+++ src/components/Pages/Quiz/MiniPlayer/MiniPlayer.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import miniPlayerImg from '../../../assets/images/3.jpg';
 import sou from './MiniPlayer.module.css';
 
 function MiniPlayer() {
+    const [status, setStatus] = useState(false);
+    const buttonRef = useRef();
+
+    const toggleMiniPlayer = () => {
+        if (!status) {
+            buttonRef.current.classList.remove(sou.floatingBtn);
+            setStatus(true);
+        } else {
+            buttonRef.current.classList.add(sou.floatingBtn);
+            setStatus(false);
+        }
+    }
+
     return (
-        <div className={`${sou.miniPlayer} ${sou.floatingBtn}`}>
-            <span className={`material-icons-outlined ${sou.open}`}> play_circle_filled </span>
-            <span className={`material-icons-outlined ${sou.close}`}> close </span>
+        <div
+            className={`${sou.miniPlayer} ${sou.floatingBtn}`}
+            ref={buttonRef}
+            onClick={toggleMiniPlayer}
+        >
+            <span className={`material-icons-outlined ${sou.open}`}>
+                {' '}
+                play_circle_filled{' '}
+            </span>
+            <span
+                className={`material-icons-outlined ${sou.close}`}
+                onClick={toggleMiniPlayer}
+            >
+                {' '}
+                close{' '}
+            </span>
             <img src={miniPlayerImg} alt=""/>
             <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
         </div>
